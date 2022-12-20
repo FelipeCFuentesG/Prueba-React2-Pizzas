c41ecb9ddb355e1b94411e18e8ddc8b5bfb3113a
Felipe Fuentes <felipe.c.fuentes.g@gmail.com>
2022-12-20 20:02:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyContext from "./my-context";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import Home from "./views/Home";
+import Carrito from './views/Carrito';
+import Pizza from './views/Pizza.jsx'
+import Navbar from './components/Navbar.jsx'
+
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([])
+
+
+
+  const datosPizzas = async () => {
+    const endpoint = './pizzas.json'
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    setPizzas(data)
+  }
+
+  useEffect(() => {
+    datosPizzas()
+  }, []);
+
+
+
+
+  const estadoGlobal = { pizzas }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <MyContext.Provider value={estadoGlobal}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/carrito' element={<Carrito />} />
+            <Route path='/pizza/:id' element={<Pizza />} />
+          </Routes>
+        </BrowserRouter>
+      </MyContext.Provider>
+    </>
+  )
 }
 
 export default App;
